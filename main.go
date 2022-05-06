55dce04271dbbacff44ab32659c468d995f0b5b6
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-06 23:32:57 +0530

    Convert main.go to a web based hello world app

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("Привет, world!")
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Привет, world."})
+	})
+
+	router.Run(":8080")
 }
