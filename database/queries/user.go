ec0852cf2c6bc79e65bb03c384307d862a065967
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-06 21:22:06 +0530

    Add dummy queries and tests

---
+++ database/queries/user.go
@@ -0,0 +1,15 @@
+package queries
+
+import (
+	"github.com/abhi-kr-2100/motion2/database/models"
+
+	"github.com/google/uuid"
+)
+
+func GetUserByID(id uuid.UUID) *models.User {
+	panic("queries: not implemented")
+}
+
+func GetUserByUsername(username string) *models.User {
+	panic("queries: not implemented")
+}
---
+++ database/queries/user_test.go
@@ -0,0 +1,25 @@
+package queries
+
+import (
+	"testing"
+)
+
+func TestGetUserByID(t *testing.T) {
+	t.Run("with existing user", func(t *testing.T) {
+		panic("test: not implemented")
+	})
+
+	t.Run("with non-existent user", func(t *testing.T) {
+		panic("test: not implemented")
+	})
+}
+
+func TestGetUserByUsername(t *testing.T) {
+	t.Run("with existing user", func(t *testing.T) {
+		panic("test: not implemented")
+	})
+
+	t.Run("with non-existent user", func(t *testing.T) {
+		panic("test: not implemented")
+	})
+}
