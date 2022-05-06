67043cee47112aa3b6774d5f2539640b4029ef93
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-06 20:04:30 +0530

    Add base model
    The base model, Model, will be used instead of gorm.Model. The
    only difference is that our base
    Model uses UUID instead of uint
    for ID.
    
    A base model such as this will be embedded into every non-base
    DB model.

---
+++ database/models/base.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Model struct {
+	ID uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()"`
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt
+}
