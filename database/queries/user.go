df5dfd013487eca795a0472619323ab4009faf21
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-06 22:15:23 +0530

    Implement GetUserByID along with tests

--- database/queries/user.go
+++ database/queries/user.go
@@ -1,13 +1,19 @@
 package queries
 
 import (
+	"github.com/abhi-kr-2100/motion2/database"
 	"github.com/abhi-kr-2100/motion2/database/models"
 
 	"github.com/google/uuid"
 )
 
 func GetUserByID(id uuid.UUID) (*models.User, error) {
-	panic("queries: not implemented")
+	var user models.User
+	if err := database.DB().Where("id = ?", id).First(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func GetUserByUsername(username string) (*models.User, error) {
--- database/queries/user_test.go
+++ database/queries/user_test.go
@@ -1,16 +1,74 @@
 package queries
 
 import (
+	"regexp"
 	"testing"
+
+	dbService "github.com/abhi-kr-2100/motion2/database"
+	"github.com/abhi-kr-2100/motion2/database/models"
+	"github.com/abhi-kr-2100/motion2/tests"
+
+	"github.com/google/uuid"
 )
 
 func TestGetUserByID(t *testing.T) {
+	var query = regexp.QuoteMeta(
+		`SELECT * FROM "users" WHERE id = $1 AND "users"."deleted_at" IS NULL ORDER BY "users"."id" LIMIT 1`,
+	)
+
 	t.Run("with existing user", func(t *testing.T) {
-		panic("test: not implemented")
+		gormDB, db, mock := tests.DBMocks()
+		defer db.Close()
+
+		dbService.SetCustomDB(gormDB)
+
+		mockUserID := uuid.New()
+		mockUsername := "mock-username"
+		mockPassword := "mock-password"
+		mockUser := models.NewUser(mockUsername, mockPassword)
+		mockUser.ID = mockUserID
+
+		row := mock.NewRows([]string{"id", "username", "password_hash"}).
+			AddRow(mockUserID, mockUsername, mockUser.PasswordHash)
+		mock.ExpectQuery(query).WithArgs(mockUserID).WillReturnRows(row)
+
+		user, err := GetUserByID(mockUserID)
+		if err != nil {
+			t.Errorf("unexpected error: %v", err)
+		}
+
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("unfulfilled expectations: %v", err)
+		}
+
+		if user.ID != mockUserID {
+			t.Errorf("expected user ID to be %v, got %v", mockUserID, user.ID)
+		}
+		if user.Username != mockUsername {
+			t.Errorf("expected user username to be %v, got %v", mockUsername, user.Username)
+		}
+		if !user.CanLogin(mockPassword) {
+			t.Errorf("expected user to be able to login with password %v", mockPassword)
+		}
 	})
 
 	t.Run("with non-existent user", func(t *testing.T) {
-		panic("test: not implemented")
+		gormDB, db, mock := tests.DBMocks()
+		defer db.Close()
+
+		dbService.SetCustomDB(gormDB)
+
+		mockUserID := uuid.New()
+		mock.ExpectQuery(query).WithArgs(mockUserID).WillReturnError(nil)
+
+		_, myerr := GetUserByID(mockUserID)
+		if myerr == nil {
+			t.Error("expected error, got nil")
+		}
+
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("unfulfilled expectations: %v", err)
+		}
 	})
 }
 
