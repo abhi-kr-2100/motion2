98a01348a290f7f31d2cc3d8d88cc55b8ccc7438
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-07 08:37:11 +0530

    Add doc-comments to Todo handlerfuncs

--- handlers/todo.go
+++ handlers/todo.go
@@ -2,10 +2,16 @@ package handlers
 
 import "github.com/gin-gonic/gin"
 
+// GetTodoByID returns a todo given the ID.
+//
+// GET /todos/:id
 func GetTodoByID(c *gin.Context) {
 	panic("handlers: not implemented")
 }
 
+// GetTodosByOwnerID returns todos given the owner ID.
+//
+// GET /users/:id/todos
 func GetTodosByOwnerID(c *gin.Context) {
 	panic("handlers: not implemented")
 }
