134d1078252126a617e2494a1b397a0f0352f8b9
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-07 08:43:56 +0530

    Implement GetTodoByID along with tests

--- handlers/todo.go
+++ handlers/todo.go
@@ -1,12 +1,43 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/abhi-kr-2100/motion2/database/queries"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 // GetTodoByID returns a todo given the ID.
 //
 // GET /todos/:id
 func GetTodoByID(c *gin.Context) {
-	panic("handlers: not implemented")
+	id, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+			"error": fmt.Sprintf("%s is not a valid UUID: %v", c.Param("id"), err),
+		})
+	}
+
+	todo, err := queries.GetTodoByID(id)
+	if err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.AbortWithStatusJSON(http.StatusNotFound, gin.H{
+				"error": fmt.Sprintf("todo with ID %s does not exist", id),
+			})
+			return
+		}
+
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": fmt.Sprintf("failed to get todo with ID %s: %v", id, err),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, todo)
 }
 
 // GetTodosByOwnerID returns todos given the owner ID.
--- handlers/todo_test.go
+++ handlers/todo_test.go
@@ -1,18 +1,87 @@
 package handlers
 
-import "testing"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"regexp"
+	"testing"
+
+	"github.com/abhi-kr-2100/motion2/database"
+	"github.com/abhi-kr-2100/motion2/database/models"
+	"github.com/abhi-kr-2100/motion2/tests"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 func TestGetTodoByID(t *testing.T) {
+	query := regexp.QuoteMeta(tests.Query_GetTodoByID)
+
+	r := tests.EngineMock()
+	r.GET("todos/:id", GetTodoByID)
+
 	t.Run("with an existing todo", func(t *testing.T) {
-		panic("tests: not implemented")
+		gormDB, db, mock := tests.DBMocks()
+		defer db.Close()
+
+		database.SetCustomDB(gormDB)
+
+		mockTodoID := uuid.New()
+		mockTodoTitle := "mock-todo-title"
+
+		row := mock.NewRows([]string{"id", "title"}).
+			AddRow(mockTodoID, mockTodoTitle)
+		mock.ExpectQuery(query).WithArgs(mockTodoID).WillReturnRows(row)
+
+		w := tests.PerformRequest(r, "GET", fmt.Sprintf("/todos/%s", mockTodoID), nil)
+
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("tests: failed to expect database expectations: %v", err)
+		}
+
+		if w.Code != http.StatusOK {
+			t.Errorf("tests: expected status code %d, got %d", http.StatusOK, w.Code)
+		}
+
+		var todo models.Todo
+		if err := json.Unmarshal(w.Body.Bytes(), &todo); err != nil {
+			t.Errorf("tests: failed to unmarshal response body: %v", err)
+		}
+
+		if todo.ID != mockTodoID {
+			t.Errorf("tests: expected todo ID %s, got %s", mockTodoID, todo.ID)
+		}
+		if todo.Title != mockTodoTitle {
+			t.Errorf("tests: expected todo title %s, got %s", mockTodoTitle, todo.Title)
+		}
 	})
 
 	t.Run("with a non-existing todo", func(t *testing.T) {
-		panic("tests: not implemented")
+		gormDB, db, mock := tests.DBMocks()
+		defer db.Close()
+
+		database.SetCustomDB(gormDB)
+
+		mockTodoID := uuid.New()
+		mock.ExpectQuery(query).WithArgs(mockTodoID).WillReturnError(gorm.ErrRecordNotFound)
+
+		w := tests.PerformRequest(r, "GET", fmt.Sprintf("/todos/%s", mockTodoID), nil)
+
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("tests: failed to expect database expectations: %v", err)
+		}
+
+		if w.Code != http.StatusNotFound {
+			t.Errorf("tests: expected status code %d, got %d", http.StatusNotFound, w.Code)
+		}
 	})
 
 	t.Run("with an invalid todo ID", func(t *testing.T) {
-		panic("tests: not implemented")
+		w := tests.PerformRequest(r, "GET", "/todos/invalid-id", nil)
+
+		if w.Code != http.StatusBadRequest {
+			t.Errorf("tests: expected status code %d, got %d", http.StatusBadRequest, w.Code)
+		}
 	})
 }
 
