6b9d87dcdc81b1a81faba23a45b8791d1f1d58a0
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-09 22:08:51 +0530

    Implement the Todo Container component with tests

--- motion2-frontend/src/app/todo-container/todo-container.component.ts
+++ motion2-frontend/src/app/todo-container/todo-container.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { TodoService } from '../todo.service';
+import Todo from '../../models/todo';
+
 @Component({
   selector: 'app-todo-container',
   templateUrl: './todo-container.component.html',
-  styleUrls: ['./todo-container.component.less']
+  styleUrls: ['./todo-container.component.less'],
 })
 export class TodoContainerComponent implements OnInit {
+  todos: Todo[] = [];
 
-  constructor() { }
+  constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
+    this.todoService.getTodos().subscribe((todos) => {
+      this.todos = todos;
+    });
   }
-
 }
