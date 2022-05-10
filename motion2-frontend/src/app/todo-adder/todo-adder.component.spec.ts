7f52bcd19e39de140a892ec44b76eefc7906cbe0
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-10 11:56:17 +0530

    Implement TodoAdder's component

--- motion2-frontend/src/app/todo-adder/todo-adder.component.spec.ts
+++ motion2-frontend/src/app/todo-adder/todo-adder.component.spec.ts
@@ -1,16 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { v4 as uuid4 } from 'uuid';
+
 import { TodoAdderComponent } from './todo-adder.component';
+import { TodoService } from '../todo.service';
+import Todo from '../../models/todo';
+import { of } from 'rxjs';
 
 describe('TodoAdderComponent', () => {
   let component: TodoAdderComponent;
   let fixture: ComponentFixture<TodoAdderComponent>;
 
+  let todoServiceSpy = jasmine.createSpyObj('TodoService', ['addTodo']);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TodoAdderComponent ]
-    })
-    .compileComponents();
+      declarations: [TodoAdderComponent],
+      providers: [{ provide: TodoService, useValue: todoServiceSpy }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +28,27 @@ describe('TodoAdderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add todo', (done: DoneFn) => {
+    const title = 'title';
+    const mockTodo: Todo = {
+      ID: uuid4(),
+      Title: title,
+      IsCompleted: false,
+      OwnerID: uuid4(),
+    };
+    todoServiceSpy.addTodo.and.returnValue(of(mockTodo));
+
+    component.addTodo(title).subscribe({
+      next: (todo) => {
+        expect(todo).toEqual(mockTodo);
+        done();
+      },
+      error: done.fail,
+    });
+
+    expect(todoServiceSpy.addTodo).toHaveBeenCalledWith(title);
+    expect(todoServiceSpy.addTodo.calls.count()).toBe(1);
   });
 });
--- motion2-frontend/src/app/todo-adder/todo-adder.component.ts
+++ motion2-frontend/src/app/todo-adder/todo-adder.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { TodoService } from '../todo.service';
+import Todo from '../../models/todo';
 
 @Component({
   selector: 'app-todo-adder',
   templateUrl: './todo-adder.component.html',
-  styleUrls: ['./todo-adder.component.less']
+  styleUrls: ['./todo-adder.component.less'],
 })
 export class TodoAdderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  addTodo(title: string): Observable<Todo> {
+    return this.todoService.addTodo(title);
   }
 
+  constructor(private todoService: TodoService) {}
+
+  ngOnInit(): void {}
 }
