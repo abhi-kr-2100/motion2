65e61d88930296b49fc912486b41cac880f3b52c
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-12 07:39:21 +0530

    Add TodoApp and Login to routes

--- motion2-frontend/src/app/app-routing.module.ts
+++ motion2-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { TodoAppComponent } from './todo-app/todo-app.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: '', component: TodoAppComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
