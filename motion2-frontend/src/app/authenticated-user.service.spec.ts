4471a91946e0328d83027cee6cd318bae56634df
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-12 17:39:00 +0530

    Add failing unit tests for AuthenticatedUser service

--- motion2-frontend/src/app/authenticated-user.service.spec.ts
+++ motion2-frontend/src/app/authenticated-user.service.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthenticatedUserService } from './authenticated-user.service';
 
+import { LoggedInUser } from '../models/user';
+
 describe('AuthenticatedUserService', () => {
   let service: AuthenticatedUserService;
 
@@ -12,5 +14,17 @@ describe('AuthenticatedUserService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should set a logged in user', () => {
+    expect(false).toBeTrue();
+  });
+
+  it('should return the user when logged in', () => {
+    expect(false).toBeTrue();
+  });
+
+  it('should return null when not logged in', () => {
+    expect(false).toBeTrue();
   });
 });
