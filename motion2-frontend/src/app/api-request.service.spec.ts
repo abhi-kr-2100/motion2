1753a6eb710d1a5f9b59555ebac3fcfa47728940
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-12 19:00:50 +0530

    Implement API request methods along with unit tests

--- motion2-frontend/src/app/api-request.service.spec.ts
+++ motion2-frontend/src/app/api-request.service.spec.ts
@@ -1,23 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
+import { LoggedInUser } from 'src/models/user';
 
 import { ApiRequestService } from './api-request.service';
+import { AuthenticatedUserService } from './authenticated-user.service';
 
 describe('ApiRequestService', () => {
   let service: ApiRequestService;
-  let httpClientSpy = jasmine.createSpyObj('HttpClient', [
-    'get',
-    'post',
-    'put',
-    'delete',
-  ]);
+  let authenticatedUserSpy: jasmine.SpyObj<any>;
+  let httpClientSpy: jasmine.SpyObj<any>;
 
   beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', [
+      'get',
+      'post',
+      'put',
+      'delete',
+    ]);
+
+    authenticatedUserSpy = jasmine.createSpyObj('AuthenticatedUser', [
+      'getUser',
+    ]);
+
     TestBed.configureTestingModule({
       providers: [
         ApiRequestService,
         { provide: HttpClient, useValue: httpClientSpy },
+        { provide: AuthenticatedUserService, useValue: authenticatedUserSpy },
       ],
     });
     service = TestBed.inject(ApiRequestService);
@@ -56,19 +66,208 @@ describe('ApiRequestService', () => {
       .toBe(stubVal);
   });
 
-  it('should make a get request', () => {
-    expect(false).toBeTruthy();
+  it('should not make any request if no user is logged in', () => {
+    authenticatedUserSpy.getUser.and.returnValue(null);
+
+    service.get('/api/test').subscribe({
+      next: () => {
+        expect(true).withContext('should not be called').toBeFalse();
+      },
+    });
+
+    service.post('/api/test').subscribe({
+      next: () => {
+        expect(true).withContext('should not be called').toBeFalse();
+      },
+    });
+
+    service.put('/api/test').subscribe({
+      next: () => {
+        expect(true).withContext('should not be called').toBeFalse();
+      },
+    });
+
+    service.delete('/api/test').subscribe({
+      next: () => {
+        expect(true).withContext('should not be called').toBeFalse();
+      },
+    });
+
+    expect(authenticatedUserSpy.getUser).toHaveBeenCalledTimes(4);
+
+    expect(httpClientSpy.get)
+      .withContext('spy was not called')
+      .not.toHaveBeenCalled();
+    expect(httpClientSpy.post)
+      .withContext('spy was not called')
+      .not.toHaveBeenCalled();
+    expect(httpClientSpy.put)
+      .withContext('spy was not called')
+      .not.toHaveBeenCalled();
+    expect(httpClientSpy.delete)
+      .withContext('spy was not called')
+      .not.toHaveBeenCalled();
   });
 
-  it('should make a post request', () => {
-    expect(false).toBeTruthy();
+  it('should make a get request with authenticated user', () => {
+    const user: LoggedInUser = {
+      username: 'a-username',
+      password: 'a-password',
+    };
+
+    const stubObj = { message: 'success' };
+    const stubVal = of(stubObj);
+
+    authenticatedUserSpy.getUser.and.returnValue(user);
+    httpClientSpy.get.and.returnValue(stubVal);
+
+    service.get('/api/test').subscribe({
+      next: (val) => {
+        expect(val).toEqual(stubObj);
+      },
+      error: () => {
+        expect(false).withContext('should not be called').toBeTrue();
+      },
+    });
+
+    expect(authenticatedUserSpy.getUser)
+      .withContext('authenticatedUserSpy was called once')
+      .toHaveBeenCalledTimes(1);
+    expect(httpClientSpy.get)
+      .withContext('httpClientSpy was called once')
+      .toHaveBeenCalledTimes(1);
+    expect(httpClientSpy.get.calls.mostRecent().args[0].endsWith('/api/test'))
+      .withContext('spy was called with given url')
+      .toBeTrue();
+    expect(httpClientSpy.get.calls.mostRecent().args[1].headers.Username)
+      .withContext('spy was called with given value of username')
+      .toBe(user.username);
+    expect(httpClientSpy.get.calls.mostRecent().args[1].headers.Password)
+      .withContext('spy was called with given value of password')
+      .toBe(user.password);
   });
 
-  it('should make a put request', () => {
-    expect(false).toBeTruthy();
+  it('should make a post request with authenticated user', () => {
+    const user: LoggedInUser = {
+      username: 'a-username',
+      password: 'a-password',
+    };
+
+    const stubObj = { message: 'success' };
+    const stubVal = of(stubObj);
+
+    authenticatedUserSpy.getUser.and.returnValue(user);
+    httpClientSpy.post.and.returnValue(stubVal);
+
+    const stubBody = { test: 'test' };
+    service.post('/api/test', stubBody).subscribe({
+      next: (val) => {
+        expect(val).toEqual(stubObj);
+      },
+      error: () => {
+        expect(false).withContext('should not be called').toBeTrue();
+      },
+    });
+
+    expect(authenticatedUserSpy.getUser)
+      .withContext('authenticatedUserSpy was called once')
+      .toHaveBeenCalledTimes(1);
+    expect(httpClientSpy.post)
+      .withContext('httpClientSpy was called once')
+      .toHaveBeenCalledTimes(1);
+    expect(httpClientSpy.post.calls.mostRecent().args[0].endsWith('/api/test'))
+      .withContext('spy was called with given url')
+      .toBeTrue();
+    expect(httpClientSpy.post.calls.mostRecent().args[1])
+      .withContext('spy was called with given body')
+      .toEqual(stubBody);
+    expect(httpClientSpy.post.calls.mostRecent().args[2].headers.Username)
+      .withContext('spy was called with given value of username')
+      .toBe(user.username);
+    expect(httpClientSpy.post.calls.mostRecent().args[2].headers.Password)
+      .withContext('spy was called with given value of password')
+      .toBe(user.password);
   });
 
-  it('should make a delete request', () => {
-    expect(false).toBeTruthy();
+  it('should make a put request with authenticated user', () => {
+    const user: LoggedInUser = {
+      username: 'a-username',
+      password: 'a-password',
+    };
+
+    const stubObj = { message: 'success' };
+    const stubVal = of(stubObj);
+
+    authenticatedUserSpy.getUser.and.returnValue(user);
+    httpClientSpy.put.and.returnValue(stubVal);
+
+    const stubBody = { test: 'test' };
+    service.put('/api/test', stubBody).subscribe({
+      next: (val) => {
+        expect(val).toEqual(stubObj);
+      },
+      error: () => {
+        expect(false).withContext('should not be called').toBeTrue();
+      },
+    });
+
+    expect(authenticatedUserSpy.getUser)
+      .withContext('authenticatedUserSpy was called once')
+      .toHaveBeenCalledTimes(1);
+    expect(httpClientSpy.put)
+      .withContext('httpClientSpy was called once')
+      .toHaveBeenCalledTimes(1);
+    expect(httpClientSpy.put.calls.mostRecent().args[0].endsWith('/api/test'))
+      .withContext('spy was called with given url')
+      .toBeTrue();
+    expect(httpClientSpy.put.calls.mostRecent().args[1])
+      .withContext('spy was called with given body')
+      .toEqual(stubBody);
+    expect(httpClientSpy.put.calls.mostRecent().args[2].headers.Username)
+      .withContext('spy was called with given value of username')
+      .toBe(user.username);
+    expect(httpClientSpy.put.calls.mostRecent().args[2].headers.Password)
+      .withContext('spy was called with given value of password')
+      .toBe(user.password);
+  });
+
+  it('should make a delete request with authenticated user', () => {
+    const user: LoggedInUser = {
+      username: 'a-username',
+      password: 'a-password',
+    };
+
+    const stubObj = { message: 'success' };
+    const stubVal = of(stubObj);
+
+    authenticatedUserSpy.getUser.and.returnValue(user);
+    httpClientSpy.delete.and.returnValue(stubVal);
+
+    service.delete('/api/test').subscribe({
+      next: (val) => {
+        expect(val).toEqual(stubObj);
+      },
+      error: () => {
+        expect(false).withContext('should not be called').toBeTrue();
+      },
+    });
+
+    expect(authenticatedUserSpy.getUser)
+      .withContext('authenticatedUserSpy was called once')
+      .toHaveBeenCalledTimes(1);
+    expect(httpClientSpy.delete)
+      .withContext('httpClientSpy was called once')
+      .toHaveBeenCalledTimes(1);
+    expect(
+      httpClientSpy.delete.calls.mostRecent().args[0].endsWith('/api/test')
+    )
+      .withContext('spy was called with given url')
+      .toBeTrue();
+    expect(httpClientSpy.delete.calls.mostRecent().args[1].headers.Username)
+      .withContext('spy was called with given value of username')
+      .toBe(user.username);
+    expect(httpClientSpy.delete.calls.mostRecent().args[1].headers.Password)
+      .withContext('spy was called with given value of password')
+      .toBe(user.password);
   });
 });
--- motion2-frontend/src/app/api-request.service.ts
+++ motion2-frontend/src/app/api-request.service.ts
@@ -1,16 +1,72 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { AuthenticatedUserService } from './authenticated-user.service';
+import { LoggedInUser } from '../models/user';
+import { throwError } from 'rxjs';
+
 @Injectable({
   providedIn: 'root',
 })
 export class ApiRequestService {
   baseURL = 'http://localhost:4200/';
 
-  getWithHeaders(url: string, headers: any) {
-    const fullURL = this.baseURL + url;
-    return this.http.get(fullURL, { headers });
+  _getOptions() {
+    const user: LoggedInUser | null = this.authenticatedUser.getUser();
+    if (user === null) {
+      return null;
+    }
+
+    return { headers: { Username: user.username, Password: user.password } };
   }
 
-  constructor(private http: HttpClient) {}
+  getWithHeaders(url: string, headers: any) {
+    const fullURL = this.baseURL + url;
+    return this.http.get(fullURL, { headers: headers });
+  }
+
+  get(url: string) {
+    const options = this._getOptions();
+    if (options === null) {
+      return throwError(() => new Error('No authenticated user found'));
+    }
+
+    const fullURL = this.baseURL + url;
+    return this.http.get(fullURL, options);
+  }
+
+  post(url: string, body?: any) {
+    const options = this._getOptions();
+    if (options === null) {
+      return throwError(() => new Error('No authenticated user found'));
+    }
+
+    const fullURL = this.baseURL + url;
+    return this.http.post(fullURL, body, options);
+  }
+
+  put(url: string, body?: any) {
+    const options = this._getOptions();
+    if (options === null) {
+      return throwError(() => new Error('No authenticated user found'));
+    }
+
+    const fullURL = this.baseURL + url;
+    return this.http.put(fullURL, body, options);
+  }
+
+  delete(url: string) {
+    const options = this._getOptions();
+    if (options === null) {
+      return throwError(() => new Error('No authenticated user found'));
+    }
+
+    const fullURL = this.baseURL + url;
+    return this.http.delete(fullURL, options);
+  }
+
+  constructor(
+    private http: HttpClient,
+    private authenticatedUser: AuthenticatedUserService
+  ) {}
 }
