539fe35499549b837f640cfdb945994d539a9c50
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-12 07:59:23 +0530

    Implement a dummy login form

--- motion2-frontend/src/app/login/login.component.ts
+++ motion2-frontend/src/app/login/login.component.ts
@@ -1,15 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.less']
+  styleUrls: ['./login.component.less'],
 })
 export class LoginComponent implements OnInit {
+  username: string = '';
+  password: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  login() {
+    // TODO: Implement login
+    console.log('Trying to log in...');
   }
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {}
 }
