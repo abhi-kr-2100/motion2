317cbc064a73cfed822684365fe34d722e40eceb
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-12 17:08:51 +0530

    Add failing tests for API Request service

--- motion2-frontend/src/app/api-request.service.spec.ts
+++ motion2-frontend/src/app/api-request.service.spec.ts
@@ -13,4 +13,24 @@ describe('ApiRequestService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should not make a get request given headers', () => {
+    expect(false).toBeTruthy();
+  });
+
+  it('should make a get request', () => {
+    expect(false).toBeTruthy();
+  });
+
+  it('should make a post request', () => {
+    expect(false).toBeTruthy();
+  });
+
+  it('should make a put request', () => {
+    expect(false).toBeTruthy();
+  });
+
+  it('should make a delete request', () => {
+    expect(false).toBeTruthy();
+  });
 });
