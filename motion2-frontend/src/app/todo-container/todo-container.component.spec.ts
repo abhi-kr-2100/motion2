b0ecfb005dc686f9282bc6328d6541e8cf94a1eb
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-14 14:10:38 +0530

    Collect todos on init
    Add tests to ensure this behavior.

--- motion2-frontend/src/app/todo-container/todo-container.component.spec.ts
+++ motion2-frontend/src/app/todo-container/todo-container.component.spec.ts
@@ -1,16 +1,92 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { v4 as uuid4 } from 'uuid';
+import { of } from 'rxjs';
+
+import { Todo } from 'src/models/todo';
+import { ApiRequestService } from '../api-request.service';
 import { TodoContainerComponent } from './todo-container.component';
 
 describe('TodoContainerComponent', () => {
   let component: TodoContainerComponent;
   let fixture: ComponentFixture<TodoContainerComponent>;
+  let http: jasmine.SpyObj<ApiRequestService>;
+
+  const _currentOwnerID = uuid4();
+  const mockTodos: Todo[] = [
+    {
+      ID: uuid4(),
+      Title: 'Todo 0',
+      IsCompleted: false,
+      OwnerID: _currentOwnerID,
+    },
+    {
+      ID: uuid4(),
+      Title: 'Todo 1',
+      IsCompleted: true,
+      OwnerID: _currentOwnerID,
+    },
+    {
+      ID: uuid4(),
+      Title: 'Todo 2',
+      IsCompleted: false,
+      OwnerID: _currentOwnerID,
+    },
+    {
+      ID: uuid4(),
+      Title: 'Todo 3',
+      IsCompleted: true,
+      OwnerID: _currentOwnerID,
+    },
+    {
+      ID: uuid4(),
+      Title: 'Todo 4',
+      IsCompleted: false,
+      OwnerID: _currentOwnerID,
+    },
+    {
+      ID: uuid4(),
+      Title: 'Todo 5',
+      IsCompleted: true,
+      OwnerID: _currentOwnerID,
+    },
+    {
+      ID: uuid4(),
+      Title: 'Todo 6',
+      IsCompleted: false,
+      OwnerID: _currentOwnerID,
+    },
+    {
+      ID: uuid4(),
+      Title: 'Todo 7',
+      IsCompleted: true,
+      OwnerID: _currentOwnerID,
+    },
+    {
+      ID: uuid4(),
+      Title: 'Todo 8',
+      IsCompleted: false,
+      OwnerID: _currentOwnerID,
+    },
+    {
+      ID: uuid4(),
+      Title: 'Todo 9',
+      IsCompleted: true,
+      OwnerID: _currentOwnerID,
+    },
+  ];
 
   beforeEach(async () => {
+    http = jasmine.createSpyObj<ApiRequestService>('ApiRequestService', [
+      'get',
+    ]);
+
+    http.get.and.returnValue(of(mockTodos));
+
     await TestBed.configureTestingModule({
-      declarations: [ TodoContainerComponent ]
-    })
-    .compileComponents();
+      declarations: [TodoContainerComponent],
+      providers: [{ provide: ApiRequestService, useValue: http }],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +97,10 @@ describe('TodoContainerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should collect todos on init', () => {
+    expect(http.get).toHaveBeenCalledTimes(1);
+    expect(component.todos).toEqual(mockTodos);
   });
 });
--- motion2-frontend/src/app/todo-container/todo-container.component.ts
+++ motion2-frontend/src/app/todo-container/todo-container.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/models/todo';
+
+import { ApiRequestService } from '../api-request.service';
 
 @Component({
   selector: 'app-todo-container',
   templateUrl: './todo-container.component.html',
-  styleUrls: ['./todo-container.component.less']
+  styleUrls: ['./todo-container.component.less'],
 })
 export class TodoContainerComponent implements OnInit {
+  todos: Todo[] = [];
 
-  constructor() { }
+  constructor(private http: ApiRequestService) {}
 
   ngOnInit(): void {
+    this.http.get<Todo[]>('/todos').subscribe({
+      next: (todos) => {
+        this.todos = todos;
+      },
+      error: (err) => {
+        alert(`Couldn't get todos: ${err}`);
+      },
+    });
   }
-
 }
