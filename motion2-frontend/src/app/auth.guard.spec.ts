1ce8d2ffaa2e8a7cea115acfb4ff961d8d7f7f9f
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-15 12:42:23 +0530

    Implement the auth guard with tests

--- motion2-frontend/src/app/auth.guard.spec.ts
+++ motion2-frontend/src/app/auth.guard.spec.ts
@@ -1,16 +1,83 @@
 import { TestBed } from '@angular/core/testing';
+import {
+  ActivatedRouteSnapshot,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+
+import { of, throwError } from 'rxjs';
 
 import { AuthGuard } from './auth.guard';
+import { AuthenticatedUserService } from './authenticated-user.service';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
+  let mockRouterStateSnapshot: jasmine.SpyObj<RouterStateSnapshot>;
+  let routerSpy: jasmine.SpyObj<Router>;
+  let authenticatedUserSpy: jasmine.SpyObj<AuthenticatedUserService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockRouterStateSnapshot = jasmine.createSpyObj('RouterStateSnapshot', [
+      'toString',
+    ]);
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    authenticatedUserSpy = jasmine.createSpyObj('AuthenticatedUserService', [
+      'verify',
+    ]);
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Router, useValue: routerSpy },
+        { provide: AuthenticatedUserService, useValue: authenticatedUserSpy },
+      ],
+    });
     guard = TestBed.inject(AuthGuard);
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('should redirect for non-logged in users', () => {
+    authenticatedUserSpy.verify.and.returnValue(
+      throwError(() => 'User not logged in')
+    );
+    routerSpy.navigate.and.returnValue(new Promise(() => true));
+
+    guard
+      .canActivate(new ActivatedRouteSnapshot(), mockRouterStateSnapshot)
+      .subscribe({
+        next: (v) => {
+          expect(v).toBeFalse();
+        },
+        error: (_) => {
+          expect(false).toBeTrue();
+        },
+      });
+
+    expect(authenticatedUserSpy.verify).toHaveBeenCalledOnceWith();
+    expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['/login']);
+  });
+
+  it('should not redirect if user is logged in', () => {
+    authenticatedUserSpy.verify.and.returnValue(
+      of({
+        message: 'user is logged in',
+      })
+    );
+
+    guard
+      .canActivate(new ActivatedRouteSnapshot(), mockRouterStateSnapshot)
+      .subscribe({
+        next: (v) => {
+          expect(v).toBeTrue();
+        },
+        error: (_) => {
+          expect(false).toBeTrue();
+        },
+      });
+
+    expect(authenticatedUserSpy.verify).toHaveBeenCalledOnceWith();
+    expect(routerSpy.navigate).not.toHaveBeenCalled();
   });
 });
--- motion2-frontend/src/app/auth.guard.ts
+++ motion2-frontend/src/app/auth.guard.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+
+import { AuthenticatedUserService } from './authenticated-user.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private authenticatedUser: AuthenticatedUserService
+  ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    let isUserLoggedIn = new Subject<boolean>();
+    this.authenticatedUser.verify().subscribe({
+      next: (_) => isUserLoggedIn.next(true),
+      error: (_) => {
+        this.router.navigate(['/login']);
+        isUserLoggedIn.next(false);
+      },
+    });
+
+    return isUserLoggedIn.asObservable();
   }
-  
 }
