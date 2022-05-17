f4c8b8058450704424bc9697ffa50595e6d56589
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-17 21:49:00 +0530

    Remove temp code in app component

--- motion2-frontend/src/app/app-routing.module.ts
+++ motion2-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
+import { TodoAppComponent } from './todo-app/todo-app.component';
+
+const routes: Routes = [
+  { path: '', component: TodoAppComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- motion2-frontend/src/app/app.component.spec.ts
+++ motion2-frontend/src/app/app.component.spec.ts
@@ -23,15 +23,4 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     expect(component).toBeTruthy();
   });
-
-  it(`should have as title 'motion2-frontend'`, () => {
-    expect(component.title).toEqual('motion2-frontend');
-  });
-
-  it('should render title', () => {
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'motion2-frontend app is running!'
-    );
-  });
 });
