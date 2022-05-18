2f76c9213452339aa8a6a69a62c3828ec5d6c273
Abhishek Kumar <abhi.kr.2100@gmail.com>
2022-05-18 15:00:54 +0530

    Implement the createNewTodo function

--- motion2-frontend/src/app/todo-adder/todo-adder.component.spec.ts
+++ motion2-frontend/src/app/todo-adder/todo-adder.component.spec.ts
@@ -1,25 +1,95 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { of, throwError } from 'rxjs';
+import { v4 as uuid4 } from 'uuid';
+
+import { ApiRequestService } from '../api-request.service';
+import { AuthenticatedUserService } from '../authenticated-user.service';
 import { TodoAdderComponent } from './todo-adder.component';
 
 describe('TodoAdderComponent', () => {
   let component: TodoAdderComponent;
   let fixture: ComponentFixture<TodoAdderComponent>;
+  let apiRequestSpy: jasmine.SpyObj<ApiRequestService>;
+  let authenticatedUserSpy: jasmine.SpyObj<AuthenticatedUserService>;
+  const mockUserID = uuid4();
 
   beforeEach(async () => {
+    apiRequestSpy = jasmine.createSpyObj('ApiRequestService', ['post']);
+    authenticatedUserSpy = jasmine.createSpyObj('AuthenticatedUserService', [
+      'getUser',
+    ]);
     await TestBed.configureTestingModule({
-      declarations: [ TodoAdderComponent ]
-    })
-    .compileComponents();
+      declarations: [TodoAdderComponent],
+      providers: [
+        { provide: ApiRequestService, useValue: apiRequestSpy },
+        { provide: AuthenticatedUserService, useValue: authenticatedUserSpy },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoAdderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    authenticatedUserSpy.getUser.and.returnValue({
+      id: mockUserID,
+      username: 'test',
+      password: 'lalala',
+    });
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add component when title is given', () => {
+    apiRequestSpy.post.and.returnValue(
+      of({
+        message: 'created',
+      })
+    );
+
+    const todoForm = {
+      Title: 'mock title',
+      IsCompleted: false,
+      OwnerID: mockUserID,
+    };
+
+    component.newTodoTitle = 'mock title';
+    component.createNewTodo();
+
+    expect(authenticatedUserSpy.getUser).toHaveBeenCalled();
+    expect(apiRequestSpy.post).toHaveBeenCalledOnceWith('/todos', todoForm);
+    expect(component.newTodoTitle).toBe('');
+  });
+
+  it('should not add component if title is not given', () => {
+    component.newTodoTitle = '';
+    component.createNewTodo();
+
+    expect(authenticatedUserSpy.getUser).not.toHaveBeenCalled();
+    expect(apiRequestSpy.post).not.toHaveBeenCalled();
+    expect(component.newTodoTitle).toBe('');
+  });
+
+  it('should preserve old title on failure', () => {
+    apiRequestSpy.post.and.returnValue(
+      throwError(() => new Error('mock error'))
+    );
+
+    component.newTodoTitle = 'mock title';
+    component.createNewTodo();
+
+    const todoForm = {
+      Title: 'mock title',
+      IsCompleted: false,
+      OwnerID: mockUserID,
+    };
+
+    expect(authenticatedUserSpy.getUser).toHaveBeenCalled();
+    expect(apiRequestSpy.post).toHaveBeenCalledOnceWith('/todos', todoForm);
+    expect(component.newTodoTitle).toBe('mock title');
   });
 });
--- motion2-frontend/src/app/todo-adder/todo-adder.component.ts
+++ motion2-frontend/src/app/todo-adder/todo-adder.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiRequestService } from '../api-request.service';
+import { AuthenticatedUserService } from '../authenticated-user.service';
+
 @Component({
   selector: 'app-todo-adder',
   templateUrl: './todo-adder.component.html',
-  styleUrls: ['./todo-adder.component.less']
+  styleUrls: ['./todo-adder.component.less'],
 })
 export class TodoAdderComponent implements OnInit {
+  newTodoTitle: string = '';
 
-  constructor() { }
+  createNewTodo() {
+    if (this.newTodoTitle.trim() === '') {
+      return;
+    }
 
-  ngOnInit(): void {
+    const todoForm = {
+      Title: this.newTodoTitle,
+      IsCompleted: false,
+      // getUser should always return a user object
+      OwnerID: this.authenticatedUser.getUser()?.id,
+    };
+
+    this.http.post('/todos', todoForm).subscribe({
+      next: (_) => {
+        this.newTodoTitle = '';
+        // TODO: Add the new todo to the list of all available todos
+      },
+      error: (err) => {
+        alert(
+          `Couldn't add new todo ${this.newTodoTitle}: ${JSON.stringify(err)}`
+        );
+      },
+    });
   }
 
+  constructor(
+    private http: ApiRequestService,
+    private authenticatedUser: AuthenticatedUserService
+  ) {}
+
+  ngOnInit(): void {}
 }
